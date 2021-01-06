c751bbe907bd872193b309cf017938d31301c0f2
Jack Hubert <jphubert93@gmail.com>
2021-01-06 14:15:27 -0700

    Created User model with some seeds

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+User.destroy_all
+
+User.create username: "Jack", password: "admin"
+User.create username: "Reed", password: "admin"
+User.create username: "Jon", password: "doomslayer"
+User.create username: "Kyle", password: "password"
